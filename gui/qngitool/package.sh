b90031c1ee7c8077e8b524932c120586f14af5ee
Anders Kaestner <anderskaestner@users.noreply.github.com>
2015-08-17 12:28:07 +0000

    Adapted exception to new base class changed target name of package

--- gui/qngitool/package.sh
+++ gui/qngitool/package.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-fname=~/qtngitool_`uname -s`_`uname -m`_`date +%Y%m%d`_rev`svn info |grep Revision | sed -e 's/Revision: //'`.tar.bz2
+fname=~/QTnGITool_`uname -s`_`uname -m`_`date +%Y%m%d`_rev`svn info |grep Revision | sed -e 's/Revision: //'`.tar.bz2
 
 echo $fname
 pushd .
 cd ~/repos/ngi
-#tar -jcvhf $fname muhrec3
 tar -jcvf $fname qngitool
 popd
  
