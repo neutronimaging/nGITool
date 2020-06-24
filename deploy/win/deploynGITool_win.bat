fefc86f878d8eb521031df7c1aefad6f4e5c4aa2
Anders Kaestner <anders.kaestner@psi.ch>
2020-06-24 17:06:19 +0200

    Added packaging

--- deploy/win/deploynGITool_win.bat
+++ deploy/win/deploynGITool_win.bat
@@ -56,3 +56,12 @@ copy Qt5Charts.dll %DEST%
 
 popd
 
+for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
+set ldt=%ldt:~0,8%
+FOR /F "tokens=*" %%g IN ('git rev-parse --short HEAD') do (set tag=%%g)
+
+if exist "C:\Program Files\7-Zip\7z.exe" (
+    "C:\Program Files\7-Zip\7z.exe" a %DEST%\..\nGITool_build%tag%-%ldt%.zip %DEST%
+) else (
+    echo 7zip doesn't exist
+)
